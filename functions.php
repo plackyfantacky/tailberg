ebcceebfccb66fadd564af2cf0c952a692093225
Adam Trickett <adamt@workhouse.au>
2024-01-09 08:47:31 +0800

    customise theme.json with Tailwind equivalents

--- functions.php
+++ functions.php
@@ -19,19 +19,6 @@
         wp_enqueue_style('twple-style', get_stylesheet_directory_uri() . '/css/frontend.css', [], filemtime(get_stylesheet_directory() . '/css/frontend.css'));
     });
 
-    // wordpress customiser is pretty useless, so let's disable it
-    add_action('init', function() {
-        add_filter('map_meta_cap', function($caps = [], $cap = '', $user_id = 0, $args = []) {
-            if ($cap == 'customize') { return ['nope']; } return $caps;
-        }, 10, 4);
-    }, 999);
-
-    add_action('admin_init', function() {
-        remove_action('plugins_loaded', '_wp_customize_include', 10);
-        remove_action('admin_enqueue_scripts', '_wp_customize_loader_settings', 11);
-
-        add_action('load-customize.php', function() { wp_die('The Customizer is disabled. You shouldn\'t use it anyway! <a href="'.get_dashboard_url().'">Back to Dashboard</a>'); });
-    }, 999);
-
+    add_filter('should_load_separate_core_block_assets', '__return_true');
 
 
